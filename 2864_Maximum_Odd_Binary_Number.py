6d177500e0b73dd28ac9a757897e908983db9c9d
vaidikarya2008-cloud <vaidik.arya.2008@gmail.com>
2026-07-21 21:44:50 +0530

    Add files via upload
    Add solution for LeetCode 2864 - Maximum Odd Binary Number

---
+++ 2864_Maximum_Odd_Binary_Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        lst = list(map(int, s))
+        count1 = 0
+        count0 = 0
+
+        for i in range(len(lst)):
+            if lst[i] == 1:
+                count1 += 1
+            else:
+                count0 += 1
+
+        reduc = count1 - 1
+
+        return "1" * reduc + "0" * count0 + "1"
